2bfec7375b061022067a5cbbf7ae68bc6572bc8f
Julie Thomsen <78859162+juliestorgaardthomsen1998@users.noreply.github.com>
2022-02-03 12:53:49 +0100

    hjf

--- Hand-testing calculator/Program.cs
+++ Hand-testing calculator/Program.cs
@@ -7,12 +7,7 @@ namespace Hand_testing_calculator
         static void Main(string[] args)
         {
             Calculator calculator = new Calculator();
-            double number1 = 3;
-            double number2 = 4;
-
-            Console.WriteLine("ADD: " + calculator.Add(number1,number2) + "Subtract: " + calculator.Subtract(number1,number2) + "Multiply: " + calculator.Multiply(number1,number2) + "Power: " + calculator.Power(number1,number2));
-
-            Console.ReadKey();
+            
 
         }
     }
