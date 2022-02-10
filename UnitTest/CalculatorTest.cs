9de59972aee6274e6243074c15f77e99f49debcd
Julie Thomsen <78859162+juliestorgaardthomsen1998@users.noreply.github.com>
2022-02-10 12:25:39 +0100

    Ændret Add test i CalculatorTest

--- UnitTest/CalculatorTest.cs
+++ UnitTest/CalculatorTest.cs
@@ -12,85 +12,54 @@ namespace UnitTest
         [SetUp]
         public void Setup()
         {
+            //Arrange
             uut = new Calculator();
         }
 
-        [TestCase(2, 2)]
-        [TestCase(-1, -1)]
-        [TestCase(7, 7)]
-        [TestCase(-10, -10)]
-
-        public void Add_Add1PosAndNegNumber_ResultIsCorrect(double a, double result)
+        [Test]
+        public void Add_Add1PosNumber_ResultIs4()
         {
-            Assert.That(uut.Add(a), Is.EqualTo(result));
+            //Act
+            double number1 = uut.Add(4);
+
+            //Assert
+            Assert.That(number1, Is.EqualTo(4));
         }
 
-        //[TestCase(3, 2, 5)]
-        //[TestCase(-3, -2, -5)]
-        //[TestCase(-3, 2, -1)]
-        //[TestCase(3, -2, 1)]
-        //[TestCase(3, 0, 3)]
-        //public void Add_AddPosAndNegNumbers_ResultIsCorrect(int a, int b, int result)
-        //{
-        //    Assert.That(uut.Add(a, b), Is.EqualTo(result));
-        //}
+        [Test]
+        public void Add_Add2PosNumber_ResultIs6()
+        {
+            //Act
+            uut.Add(2);
+            double number2 = uut.Add(4);
+
+            //Assert
+            Assert.That(number2, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Add_Add1PosAnd1NegNumber_ResultIs6()
+        {
+            //Act
+            uut.Add(-2);
+            double number2 = uut.Add(8);
+
+            //Assert
+            Assert.That(number2, Is.EqualTo(6));
+        }
 
 
-        //[TestCase(3, 2, 1)]
-        //[TestCase(-3, -2, -1)]
-        //[TestCase(-3, 2, -5)]
-        //public void Subtract_SubtractPosAndNegNumbers_ResultIsCorrect(int a, int b, int result)
-        //{
-        //    Assert.That(uut.Subtract(a, b), Is.EqualTo(result));
-        //}
-
-
-        //[TestCase(3, 2, 6)]
-        //[TestCase(-3, -2, 6)]
-        //[TestCase(-3, 2, -6)]
-        //[TestCase(3, -2, -6)]
-        //[TestCase(0, -2, 0)]
-        //[TestCase(-2, 0, 0)]
-        //[TestCase(0, 0, 0)]
-        //public void Multiply_MultiplyNunmbers_ResultIsCorrect(int a, int b, int result)
-        //{
-        //    Assert.That(uut.Multiply(a, b), Is.EqualTo(result));
-        //}
-
-
-        //[TestCase(2, 3, 8)]
-        //[TestCase(2, -3, 0.125)]
-        //[TestCase(-2, -3, -0.125)]
-        //[TestCase(1, 10, 1)]
-        //[TestCase(1, -10, 1)]
-        //[TestCase(10, 0, 1)]
-        //[TestCase(4, 0.5, 2.0)]
-        //[TestCase(9, 0.5, 3.0)]
-        //public void Power_RaiseNumbers_ResultIsCorrect(double x, double exp, double result)
-        //{
-        //    Assert.That(uut.Power(x, exp), Is.EqualTo(result));
-        //}
 
         [Test]
         public void Clear_ClearAccumulator_AccumulatorIsSetToZero()
         {
+            //Act
             uut.Clear();
 
+            //Assert
             Assert.That(uut.Accumulator, Is.EqualTo(0));
         }
 
-        //[TestCase(3, 2, 1.5)]
-        //[TestCase(-3, -2, 1.5)]
-        //[TestCase(-3, 2, -1.5)]
-        //public void Test_Devide_Method(double a, double b, double result)
-        //{
-        //    Assert.That(uut.Divide(a, b), Is.EqualTo(result));
-        //}
-
-        //[TestCase(3,0)]
-        //public void Test_Devide_with_0(double a, double b)
-        //{
-        //    Assert.Throws<ArgumentOutOfRangeException> (()=>uut.Divide(a,b));
-        //}
+        
     }
 }
